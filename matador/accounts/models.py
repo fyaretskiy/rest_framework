31355de236493af1de39d461525fa3371eac2012
feliks <fyaretskiy@gmail.com>
2017-04-30 13:31:36 -0400

    Renamed Post model, add UserPost many to many relationship.

--- matador/accounts/models.py
+++ matador/accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    posts = models.ManyToManyField('posts.Post', through='posts.UserPost')
+
--- matador/posts/models.py
+++ matador/posts/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
-class Posts(models.Model):
+class Post(models.Model):
     post_id = models.IntegerField('post id')
     user_id = models.IntegerField('user id')
     title = models.TextField('title', null=True, blank=True)
     body = models.TextField('body', null=True, blank=True)
 
+
+class UserPost(models.Model):
+    """
+    Through many to many model for extra customization.
+    """
+    user_id = models.ForeignKey('accounts.User')
+    post_id = models.ForeignKey(Post)
